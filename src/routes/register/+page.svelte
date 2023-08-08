cfc8a7078ef39054eff3459d27164d5832670988
lonely-code-cube <lonely-code-cube@github.com>
2023-08-08 13:12:49 +0530

    ui components

--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,5 +1,31 @@
 <script lang="ts">
-	import Icon from '@iconify/svelte';
+	import TextInput from '$lib/components/ui/TextInput.svelte';
+	import PasswordInput from '$lib/components/ui/PasswordInput.svelte';
+	import EmailInput from '$lib/components/ui/EmailInput.svelte';
+
+	let disabled = true;
+	let checking_username = false;
+
+	let email: string | null;
+	let username: string | null;
+	let password: string | null;
+
+	function onUpdate() {
+		if (email && username && password) {
+			disabled = false;
+		} else {
+			disabled = true;
+		}
+	}
+	function onUsernameUpdate() {
+		checking_username = false;
+		setTimeout(() => {
+			checking_username = false;
+		}, 1000);
+	}
+
+	$: email, username, password, onUpdate();
+	$: username, onUsernameUpdate();
 </script>
 
 <svelte:head>
@@ -10,27 +36,16 @@
 	<div class="w-full md:w-96 lg:w-[500px] bg-base-200 p-5 shadow-lg shadow-base-300 mx-5">
 		<h2 class="font-bold text-3xl">Register</h2>
 		<form class="flex flex-col gap-2 mt-5">
-			<input
-				id="username"
-				placeholder="Username"
-				class="border-b border-neutral bg-transparent pt-4 pb-1.5 text-sm outline outline-0 transition-all duration-200 placeholder-shown:border-blue-gray-200 focus:border-primary focus:outline-0"
-				type="text"
-			/>
-			<input
-				placeholder="Email"
-				class="border-b border-neutral bg-transparent pt-4 pb-1.5 text-sm outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-primary focus:outline-0"
-				type="text"
-			/><input
-				placeholder="Passowrd"
-				class="border-b border-neutral bg-transparent pt-4 pb-1.5 text-sm outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-primary focus:outline-0"
-				type="text"
-			/>
-			<input
-				placeholder="Confirm Password"
-				class="border-b border-neutral bg-transparent pt-4 pb-1.5 text-sm outline outline-0 transition-all placeholder-shown:border-blue-gray-200 focus:border-primary focus:outline-0"
-				type="text"
-			/>
-			<button class="btn btn-primary mt-5" type="submit">Create Account</button>
+			<EmailInput bind:value={email} placeholder="Email" />
+			<TextInput bind:value={username} placeholder="Username">
+				<svelte:fragment slot="icon">
+					{#if checking_username}
+						<span class="loading" />
+					{/if}
+				</svelte:fragment>
+			</TextInput>
+			<PasswordInput bind:value={password} placeholder="Password" />
+			<button {disabled} class="btn btn-primary mt-5" type="submit">Create Account</button>
 		</form>
 		<div class="mt-2 mb-5">
 			Or just <a class="link link-primary" href="/login">login</a>
