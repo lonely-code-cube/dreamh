2e9cc1583281d28b52331b0319908f4fd886c4da
midatindex0 <83584402+midatindex0@users.noreply.github.com>
2023-08-13 23:03:55 +0530

    summoned devil

--- src/lib/components/ui/Medias.svelte
+++ src/lib/components/ui/Medias.svelte
@@ -1,10 +1,24 @@
 <script lang="ts">
 	export let medias: string[];
+
+	import Carousel from 'svelte-carousel';
+	import { browser } from '$app/environment';
 </script>
 
 <div class="relative">
-	<img src={medias[0]} alt="" />
-	<div class="absolute top-2 right-2 bg-base-200 bg-opacity-70 px-2 rounded shadow-xl text-sm">
-		1/1
-	</div>
+	{#if browser}
+		<Carousel>
+			<div>yo 0</div>
+			<div>yo 1</div>
+			<div>yo 2</div>
+			<div let:showPage let:pagesCount slot="dots">
+				{#each Array(pagesCount) as _, pageIndex (pageIndex)}
+					<button on:click={() => showPage(pageIndex)}>{pageIndex}</button>
+				{/each}
+			</div>
+		</Carousel>
+		<div class="absolute top-2 right-2 bg-base-200 bg-opacity-70 px-2 rounded shadow-xl text-sm">
+			1/1
+		</div>
+	{/if}
 </div>
