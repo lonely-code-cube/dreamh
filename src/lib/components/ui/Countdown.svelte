d62e9df646f383447b5bcea9fd1957551ff44cee
midatindex0 <midatindex0@users.noreply.github.com>
2023-12-19 14:26:22 +0530

    airing shedule

---
+++ src/lib/components/ui/Countdown.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { space } from 'postcss/lib/list';
+	import { onMount } from 'svelte';
+
+	export let at: number;
+
+	let d: number;
+	let h: number;
+	let m: number;
+	let s: number;
+
+	at = new Date(at * 1000).getTime();
+	onMount(() => {
+		let timer = setInterval(() => {
+			let now = new Date().getTime();
+			let seconds = at - now;
+
+			d = Math.floor(seconds / (1000 * 60 * 60 * 24));
+			h = Math.floor((seconds % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			m = Math.floor((seconds % (1000 * 60 * 60)) / (1000 * 60));
+			s = Math.floor((seconds % (1000 * 60)) / 1000);
+
+			if (seconds < 0) {
+				clearInterval(timer);
+			}
+		}, 1000);
+	});
+</script>
+
+<span class="inline-flex gap-1">
+	{#if d > 0}
+		<span>{d}d</span>
+	{/if}
+	{#if h > 0}
+		<span>{h}h</span>
+	{/if}
+	<span class="flex">
+		{#if m > 0}
+			<span>{m}:</span>
+		{/if}
+		{#if s > 0}
+			<span>{s}</span>
+		{/if}
+	</span>
+</span>
