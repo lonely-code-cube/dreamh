931731217ac1048363d6f775c38668877607ef41
midatindex0 <midatindex0@users.noreply.github.com>
2023-12-20 11:15:40 +0530

    homepage banner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import Countdown from '$lib/components/ui/Countdown.svelte';
 	import Icon from '@iconify/svelte';
 	import { queryStore, type Client, gql } from '@urql/svelte';
+	import { slide } from 'svelte/transition';
 
 	import { getContext } from 'svelte';
 	let anilist: Client = getContext('anilist');
@@ -16,7 +17,6 @@
 						id
 						title {
 							userPreferred
-							english
 						}
 						coverImage {
 							large
@@ -31,6 +31,28 @@
 						}
 					}
 				}
+
+				upcoming: Page {
+					media(seasonYear: 2024, season: WINTER, type: ANIME) {
+						id
+						title {
+							userPreferred
+						}
+						coverImage {
+							large
+						}
+					}
+				}
+
+				trendingAnime: Page(page: 1, perPage: 7) {
+					media(sort: TRENDING_DESC, type: ANIME, isAdult: false) {
+						id
+						title {
+							userPreferred
+						}
+						bannerImage
+					}
+				}
 			}
 		`
 	});
@@ -41,8 +63,14 @@
 		}
 		return 0;
 	}
+
+	let y: number;
+	let currentBanner = 0;
+
+	$: console.log(y);
 </script>
 
+<svelte:window bind:scrollY={y} />
 <svelte:head>
 	<!-- Primary Meta Tags -->
 	<title
@@ -85,43 +113,131 @@
 	<meta property="twitter:image" content="/embed_img.png" />
 </svelte:head>
 
-<div>
-	<div class="m-3 md:mx-5">
-		<a href="/anime/airing" class="mb-3 flex justify-between items-center">
-			<div class="font-semibold text-base">AIRING ANIME</div>
-			<a href="/anime/airing" class="text-sm">View More</a>
-		</a>
-		<div class="grid grid-flow-col grid-rows-2 md:grid-rows-1 gap-3 md:gap-5 justify-between">
-			{#if $home.fetching}
-				{#each Array(7) as _}
+<div class="scroll-smooth">
+	{#if $home.fetching}
+		<div class="h-64 md:h-[30rem] skeleton" style="transform: translate(0, {y - y / 4}px);" />
+	{:else if $home.data}
+		<!-- svelte-ignore a11y-click-events-have-key-events -->
+		<!-- svelte-ignore a11y-no-static-element-interactions -->
+		<div class="h-64 md:h-[30rem] relative" style="transform: translate(0, {y - y / 4}px);">
+			{#each $home.data.trendingAnime.media as a, i}
+				{#if i == currentBanner}
 					<div
-						class="[&:nth-child(7)]:hidden md:[&:nth-child(6)]:hidden lg:[&:nth-child(6)]:block lg:[&:nth-child(7)]:block"
+						transition:slide
+						style="background-image: url({a.bannerImage});"
+						class="h-full bg-center bg-cover bg-no-repeat"
 					>
-						<EntityPreview loading={true} thumbnail={''} title={''} url={'/'} />
+						<div>
+							<div
+								style="background: linear-gradient(to bottom, oklch(var(--b1) / {y > 800
+									? 0.8
+									: y / 700}) 0%, oklch(var(--b1)) 100%);"
+								class="absolute inset-0"
+							>
+								<button
+									class:hidden={y > 100}
+									on:click={() => {
+										currentBanner = (currentBanner + 1) % $home.data.trendingAnime.media.length;
+									}}
+									class="absolute top-5 right-5 drop-shadow-[0px_0px_5px_#000] text-3xl"
+								>
+									<Icon icon="icon-park-solid:up-c" />
+								</button>
+								<button
+									class:hidden={y > 100}
+									on:click={() => {
+										currentBanner =
+											currentBanner - 1 < 0
+												? $home.data.trendingAnime.media.length - 1
+												: currentBanner - 1;
+									}}
+									class="absolute bottom-14 md:bottom-32 right-5 drop-shadow-[0px_0px_5px_#000] text-3xl"
+								>
+									<Icon icon="icon-park-solid:down-c" />
+								</button>
+							</div>
+							<div
+								class="absolute top-5 left-5 w-2/3 text-2xl md:text-5xl font-bold drop-shadow-[0px_0px_5px_#000]"
+							>
+								{a.title.userPreferred}
+							</div>
+						</div>
 					</div>
-				{/each}
-			{:else if $home.error}
-				<p>Oh no... {$home.error.message}</p>
-			{:else}
-				{#each $home.data.airs.media.sort(sortAir).slice(0, 7) as media}
-					<div
-						class="[&:nth-child(7)]:hidden md:[&:nth-child(6)]:hidden lg:[&:nth-child(6)]:block lg:[&:nth-child(7)]:block"
-					>
-						<EntityPreview
-							thumbnail={media.coverImage.large}
-							title={media.title.userPreferred}
-							url={`/anime/${media.id}`}
-						>
-							<span class="px-1 py-0.5 bg-base-100/60 rounded font-semibold" slot="corner-tl">
-								<Countdown at={media.nextAiringEpisode.airingAt} />
-							</span>
-							<span class="px-1 py-0.5 bg-base-100/60 rounded font-semibold" slot="corner-br">
-								EP {media.nextAiringEpisode.episode}
-							</span>
-						</EntityPreview>
-					</div>
-				{/each}
-			{/if}
+				{/if}
+			{/each}
 		</div>
+	{/if}
+	<div class="-translate-y-16 md:-translate-y-32 m-3 md:mx-5">
+		<div>
+			<a href="/anime/airing" class="mb-3 flex justify-between items-center">
+				<div class="font-semibold text-base">AIRING ANIME</div>
+				<a href="/anime/airing" class="text-sm">View More</a>
+			</a>
+			<div class="grid grid-flow-col grid-rows-2 md:grid-rows-1 gap-3 md:gap-5 justify-between">
+				{#if $home.fetching}
+					{#each Array(7) as _}
+						<div
+							class="[&:nth-child(7)]:hidden md:[&:nth-child(6)]:hidden lg:[&:nth-child(6)]:block lg:[&:nth-child(7)]:block"
+						>
+							<EntityPreview loading={true} thumbnail={''} title={''} url={'/'} />
+						</div>
+					{/each}
+				{:else if $home.error}
+					<p>Oh no... {$home.error.message}</p>
+				{:else}
+					{#each $home.data.airs.media.sort(sortAir).slice(0, 7) as media}
+						<div
+							class="[&:nth-child(7)]:hidden md:[&:nth-child(6)]:hidden lg:[&:nth-child(6)]:block lg:[&:nth-child(7)]:block"
+						>
+							<EntityPreview
+								thumbnail={media.coverImage.large}
+								title={media.title.userPreferred}
+								url={`/anime/${media.id}`}
+							>
+								<span class="px-1 py-0.5 bg-base-100/70 rounded font-semibold" slot="corner-tl">
+									<Countdown at={media.nextAiringEpisode.airingAt} />
+								</span>
+								<span class="px-1 py-0.5 bg-base-100/70 rounded font-semibold" slot="corner-br">
+									EP {media.nextAiringEpisode.episode}
+								</span>
+							</EntityPreview>
+						</div>
+					{/each}
+				{/if}
+			</div>
+		</div>
+		<!-- just aired -->
+		<div class="mt-4">
+			<a href="/anime/airing" class="mb-3 flex justify-between items-center">
+				<div class="font-semibold text-base">UPCOMING</div>
+				<a href="/anime/airing" class="text-sm">View More</a>
+			</a>
+			<div class="grid grid-flow-col grid-rows-2 md:grid-rows-1 gap-3 md:gap-5 justify-between">
+				{#if $home.fetching}
+					{#each Array(7) as _}
+						<div
+							class="[&:nth-child(7)]:hidden md:[&:nth-child(6)]:hidden lg:[&:nth-child(6)]:block lg:[&:nth-child(7)]:block"
+						>
+							<EntityPreview loading={true} thumbnail={''} title={''} url={'/'} />
+						</div>
+					{/each}
+				{:else if $home.error}
+					<p>Oh no... {$home.error.message}</p>
+				{:else}
+					{#each $home.data.upcoming.media.slice(0, 7) as media}
+						<div
+							class="[&:nth-child(7)]:hidden md:[&:nth-child(6)]:hidden lg:[&:nth-child(6)]:block lg:[&:nth-child(7)]:block"
+						>
+							<EntityPreview
+								thumbnail={media.coverImage.large}
+								title={media.title.userPreferred}
+								url={`/anime/${media.id}`}
+							/>
+						</div>
+					{/each}
+				{/if}
+			</div>
+		</div>
+		<div class="h-screen" />
 	</div>
 </div>
